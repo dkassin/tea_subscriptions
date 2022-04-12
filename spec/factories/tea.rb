aee79d2a7d3707280f0872cba42cb75e3355f7b9
David Kassin <david.j.kassin@gmail.com>
2022-04-12 17:14:22 -0600

    Add seed data for tests and set up requets spec

--- spec/factories/tea.rb
+++ spec/factories/tea.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :subscription do
+  factory :tea do
     title { Faker::Tea.variety }
     temperature { Faker::Number.decimal(l_digits: 2) }
     brew_time { Faker::Number.number(digits: 1) }
---
+++ spec/requests/api/v1/subscriptions_request_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+RSpec.describe 'subscription API' do
+  describe 'subscriptions' do
+    before(:each) do
+      @customers = FactoryBot.create_list(:customer, 2)
+      @subscriptions = FactoryBot.create_list(:subscription, 3, status: 'active', customer_id: @customers[0].id )
+      @subscriptions_2 = FactoryBot.create_list(:subscription, 2, status: 'cancelled', customer_id: @customers[0].id )
+      @teas = FactoryBot.create_list(:tea, 5)
+      @tea_subscription_1 = TeaSubscription.create!(subscription_id: @subscriptions[0].id, tea_id: @teas[0].id)
+      @tea_subscription_2 = TeaSubscription.create!(subscription_id: @subscriptions[0].id, tea_id: @teas[2].id)
+      @tea_subscription_3 = TeaSubscription.create!(subscription_id: @subscriptions[2].id, tea_id: @teas[1].id)
+      @tea_subscription_4 = TeaSubscription.create!(subscription_id: @subscriptions_2[0].id, tea_id: @teas[1].id)
+      @tea_subscription_5 = TeaSubscription.create!(subscription_id: @subscriptions[1].id, tea_id: @teas[4].id)
+    end
+  # before(:each) do
+    # @customer = FactoryBot.create(:customer)
+    # @customer_1 = Customer.create!(first_name: 'Chuck', last_name: 'Norris', email: 'chuck_5@gmail.com', address: '123 Fake Address')
+  # end
+    it 'An endpoint to subscribe a customer to a tea subscription' do
+
+
+    end
+
+    it 'An endpoint to cancel a customer’s tea subscription' do
+
+    end
+
+    it 'An endpoint to see all of a customer’s subsciptions (active and cancelled)' do
+
+    end 
+  end
+end
