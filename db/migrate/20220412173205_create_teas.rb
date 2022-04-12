53a5837e38f728206f0697a3391a13bf41e7601a
David Kassin <david.j.kassin@gmail.com>
2022-04-12 11:33:47 -0600

    Create database and tables

---
+++ db/migrate/20220412173205_create_teas.rb
@@ -0,0 +1,11 @@
+class CreateTeas < ActiveRecord::Migration[5.2]
+  def change
+    create_table :teas do |t|
+      t.string :title
+      t.float :temperature
+      t.integer :brew_time
+
+      t.timestamps
+    end
+  end
+end
