4add182816dfe2d075dc5c1b03c439aaedac087d
David Kassin <david.j.kassin@gmail.com>
2022-04-12 12:40:05 -0600

    Add model validation and relationships with tests

--- app/models/tea_subscription.rb
+++ app/models/tea_subscription.rb
@@ -1,4 +1,7 @@
 class TeaSubscription < ApplicationRecord
   belongs_to :subscription
   belongs_to :tea
+
+  validates_presence_of :subscription_id
+  validates_presence_of :tea_id
 end
---
+++ spec/models/tea_subscriptions_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe TeaSubscription, type: :model do
+  describe "relationships" do
+    it {should belong_to :subscription}
+    it {should belong_to :tea}
+  end
+  describe 'validations' do
+    it {should validate_presence_of :subscription_id}
+    it {should validate_presence_of :tea_id}
+  end
+end
